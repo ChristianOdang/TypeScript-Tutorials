dd8bbdd4d7e226fffc7465dd41a710a66b27ddb0
JohnsonOdang <johnnyodanny@gmail.com>
2024-02-06 03:28:37 -0800

    typescript array

--- Hello.js
+++ Hello.js
@@ -1,2 +1,9 @@
 "use strict";
-console.log("Welcome to TypeScript");
+// writing out the types
+let carName = "Benz ";
+let quoteName = "is my favourite Car";
+let stateMent = carName + quoteName;
+console.log(stateMent);
+// TypeScript Array
+const names = ["Christian", "Johnson", "Mainoo", "Rico", "Angelo"];
+const schools = ['UofA', 'Fulafia', 'UNN', 'UI'];
--- Hello.ts
+++ Hello.ts
@@ -1,1 +1,12 @@
-console.log("Welcome to TypeScript");
+// writing out the types
+let carName: string = "Benz ";
+let quoteName: string = "is my favourite Car";
+let stateMent: string = carName + quoteName;
+console.log(stateMent);
+
+// TypeScript Array
+const names: string[] = ["Christian", "Johnson", "Mainoo", "Rico", "Angelo"];
+console.log(names)
+
+const schools: string[] = ['UofA', 'Fulafia', 'UNN', 'UI']
+console.log(schools)
